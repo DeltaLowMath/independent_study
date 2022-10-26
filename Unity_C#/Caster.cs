7c417d091bf57585c14b54677205dec59ddefa90
DeltaLowMath <115683334+DeltaLowMath@users.noreply.github.com>
2022-10-26 00:04:03 -0600

    Caster.cs 1.0.2
    Fixes + float changes.

--- Unity_C#/Caster.cs
+++ Unity_C#/Caster.cs
@@ -9,22 +9,14 @@ public class Caster : MonoBehaviour
     public GameObject energyBolt;
     public GameObject energyDisc;
     public GameObject energyWall;
-    public gameObject energyOrb;
-    public gameObject energyRay;
+    public GameObject energyOrb;
+    public GameObject energyRay;
+    public float spellVelocity = 0f;
 
-    public float spellVelocity;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         SpellBook();
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             CastSpell();
         }
@@ -35,38 +27,38 @@ public class Caster : MonoBehaviour
         if (Input.GetKey(KeyCode.Alpha1))
         {
             spell = energyBall;
-            spellVelocity = 32;
+            spellVelocity = 32f;
         }
         else if (Input.GetKey(KeyCode.Alpha2))
         {
             spell = energyBolt;
-            spellVelocity = 40;
+            spellVelocity = 40f;
         }
         else if (Input.GetKey(KeyCode.Alpha3))
         {
             spell = energyDisc;
-            spellVelocity = 24;
+            spellVelocity = 24f;
         }
         else if (Input.GetKey(KeyCode.Alpha4))
         {
             spell = energyWall;
-            spellVelocity = 16;
+            spellVelocity = 4f;
         }
         else if (Input.GetKey(KeyCode.Alpha5))
         {
             spell = energyOrb;
-            spellVelocity = 8;
+            spellVelocity = 8f;
         }
         else if (Input.GetKey(KeyCode.Alpha6))
         {
             spell = energyRay;
-            spellVelocity = 0;
+            spellVelocity = 16f;
         }
     }
 
     void CastSpell()
     {
         GameObject cast = Instantiate(spell, transform.position, transform.rotation);
-        spell.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, spellVelocity, 0));
+        cast.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, (spellVelocity * 15)));
     }
 }
